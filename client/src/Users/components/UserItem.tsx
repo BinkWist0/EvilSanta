394024e15f38d45ca1b7115b7d21eaf72298c031
Ilya Ilyin <144535270+BinkWist0@users.noreply.github.com>
2023-12-07 18:28:35 +0300

    style

--- client/src/Users/components/UserItem.tsx
+++ client/src/Users/components/UserItem.tsx
@@ -3,11 +3,13 @@ import type { User } from '../UserType';
 
 const UserItem = ({ user }: { user: User }): JSX.Element => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen">
       <div>
-        <img src={user.avatar} alt="" />
-        <h3>{user.name}</h3>
-        <h3>{user.secondName}</h3>
+        <img className="rounded-full w-96 h-96" src={user.avatar} alt="" />
+      </div>
+      <div className="">
+        <h3 className="text-4xl font-normal text-gray-900">{user.name}</h3>
+        <h3 className="text-4xl font-normal text-gray-900">{user.secondName}</h3>
       </div>
     </div>
   );
