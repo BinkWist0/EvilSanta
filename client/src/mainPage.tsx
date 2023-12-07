c5b0b9abb287de2a292c563fc8e3da6b9f0927f2
Alex <sakradnes@mail.ru>
2023-12-07 17:32:53 +0300

    feat:profile component

--- client/src/mainPage.tsx
+++ client/src/mainPage.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function mainPage(): JSX.Element {
+    
   return (
     <div>
-      <h1>Главная страница</h1>
-      <Link to="/profile">
-        <button>Профиль</button>
-      </Link>
       <Link to="/register">
         <button>Регистрация</button>
-      </Link>
-      <Link to="/login">
-        <button>Авторизация</button>
       </Link>
     </div>
   );
