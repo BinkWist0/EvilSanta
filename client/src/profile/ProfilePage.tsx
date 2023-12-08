98c382100b930a54424ce0c8044b45dd05367552
Alex <sakradnes@mail.ru>
2023-12-08 13:57:58 +0300

    function random

--- client/src/profile/ProfilePage.tsx
+++ client/src/profile/ProfilePage.tsx
@@ -1,16 +1,35 @@
-
 import React, { useState } from 'react';
-import type { User } from '../Users/UserType';
-import NotUnregistered from './NotUnregistered';
+// import type { User } from '../Users/UserType';
+import { useSelector } from 'react-redux';
 import type { RootState } from '../store/store';
 
+function ProfilePage(): JSX.Element {
+  const userList = useSelector((store: RootState) => store.usersState.users);
+  const user = {
+    id: 0,
+    name: 'Vasya',
+    lastname: 'Pupkin',
+    email: 'pupkin@ya.ru',
+    description: 'Happy holidays!',
+  };
 
-function ProfilePage({ user }: { user: User }): JSX.Element {
+  const [users, setUsers] = useState(userList);
+  const [recipient, setRecipient] = useState(null);
+
+  function sortUsers(): void {
+    const sortedUsers = [...users].sort(() => Math.random() - 0.5);
+    setUsers(sortedUsers);
+    const assignments = sortedUsers.map((users, i) => {
+      const nextIndex = (i + 1) % sortedUsers.length;
+      return { userId: user.id, : sortedUsers[nextIndex] };
+    });
+    setRecipient(assignments);
+  }
+
   return (
     <div>
-      <p>...</p>
       <div>
-        <img alt="..." src={user.avatarId} />
+        <img alt="..." src={user.name} />
       </div>
       <div>
         <h1>
